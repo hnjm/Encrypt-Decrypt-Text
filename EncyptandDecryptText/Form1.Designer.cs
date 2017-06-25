37c2305b8d22afe3b224acc886e954e7192a42f6
breel93 <kolajubril@gmail.com>
2017-06-25 16:30:26 +0100

    Visual studio project

---
+++ EncyptandDecryptText/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EncyptandDecryptText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncryptBtn = new System.Windows.Forms.Button();
+            this.DecryptBtn = new System.Windows.Forms.Button();
+            this.plaintxt = new System.Windows.Forms.TextBox();
+            this.EncyptTxt = new System.Windows.Forms.TextBox();
+            this.DecryptTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EncryptBtn
+            // 
+            this.EncryptBtn.Location = new System.Drawing.Point(197, 77);
+            this.EncryptBtn.Name = "EncryptBtn";
+            this.EncryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.EncryptBtn.TabIndex = 0;
+            this.EncryptBtn.Text = "Encryption";
+            this.EncryptBtn.UseVisualStyleBackColor = true;
+            this.EncryptBtn.Click += new System.EventHandler(this.EncryptBtn_Click);
+            // 
+            // DecryptBtn
+            // 
+            this.DecryptBtn.Location = new System.Drawing.Point(197, 106);
+            this.DecryptBtn.Name = "DecryptBtn";
+            this.DecryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.DecryptBtn.TabIndex = 1;
+            this.DecryptBtn.Text = "Decryption";
+            this.DecryptBtn.UseVisualStyleBackColor = true;
+            this.DecryptBtn.Click += new System.EventHandler(this.DecryptBtn_Click);
+            // 
+            // plaintxt
+            // 
+            this.plaintxt.Location = new System.Drawing.Point(91, 53);
+            this.plaintxt.Name = "plaintxt";
+            this.plaintxt.Size = new System.Drawing.Size(100, 20);
+            this.plaintxt.TabIndex = 2;
+            // 
+            // EncyptTxt
+            // 
+            this.EncyptTxt.Location = new System.Drawing.Point(91, 79);
+            this.EncyptTxt.Name = "EncyptTxt";
+            this.EncyptTxt.Size = new System.Drawing.Size(100, 20);
+            this.EncyptTxt.TabIndex = 3;
+            // 
+            // DecryptTxt
+            // 
+            this.DecryptTxt.Location = new System.Drawing.Point(91, 108);
+            this.DecryptTxt.Name = "DecryptTxt";
+            this.DecryptTxt.Size = new System.Drawing.Size(100, 20);
+            this.DecryptTxt.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Simple text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Encrypted Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Decrypted Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DecryptTxt);
+            this.Controls.Add(this.EncyptTxt);
+            this.Controls.Add(this.plaintxt);
+            this.Controls.Add(this.DecryptBtn);
+            this.Controls.Add(this.EncryptBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncryptBtn;
+        private System.Windows.Forms.Button DecryptBtn;
+        private System.Windows.Forms.TextBox plaintxt;
+        private System.Windows.Forms.TextBox EncyptTxt;
+        private System.Windows.Forms.TextBox DecryptTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EncyptandDecryptText/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncyptandDecryptText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            desObj = Rijndael.Create();
+
+        }
+        string cipherData;
+        byte[] cipherbytes;
+        byte[] plainbytes;
+        byte[] plainbytes2;
+        byte[] plainkey;
+
+        SymmetricAlgorithm desObj;
+
+        private void EncryptBtn_Click(object sender, EventArgs e)
+        {
+            cipherData = plaintxt.Text;
+            plainbytes = Encoding.ASCII.GetBytes(cipherData);
+            plainkey = Encoding.ASCII.GetBytes("0123456789abcdef");
+            desObj.Key = plainkey;
+
+            desObj.Mode = CipherMode.CBC;
+            desObj.Padding = PaddingMode.PKCS7;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, desObj.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(plainbytes, 0, plainbytes.Length);
+            cs.Close();
+            cipherbytes = ms.ToArray();
+            ms.Close();
+            EncyptTxt.Text = Encoding.ASCII.GetString(cipherbytes);
+        }
+
+        private void DecryptBtn_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream(cipherbytes);
+            CryptoStream cs = new CryptoStream(ms, desObj.CreateDecryptor(), CryptoStreamMode.Read);
+            cs.Read(cipherbytes, 0, cipherbytes.Length);
+            plainbytes2 = ms.ToArray();
+            cs.Close();
+            ms.Close();
+            DecryptTxt.Text = Encoding.ASCII.GetString(plainbytes2);
+        }
+    }
+}
